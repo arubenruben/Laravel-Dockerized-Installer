fb0c7835db3c8ecbd6a21787eaab2ba7228c929f
Rében Almeida <ruben.almeida@innovpoint.com>
2025-08-02 12:15:34 +0100

    Add blueprint and debugbar as essential dependencies

--- install.sh
+++ install.sh
@@ -11,8 +11,20 @@ cd ./out
 # Install Laravel with React, PHPUnit, and npm
 laravel new ${APP_NAME} --react --phpunit --npm --no-interaction
 
+cd ${APP_NAME}
+
 # Install Telescope
 composer require laravel/telescope --dev && php artisan telescope:install
 
 # Install L5-Swagger
 composer require "darkaonline/l5-swagger" && php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"
+
+# Install Laravel Shift Blueprint
+composer require -W --dev laravel-shift/blueprint
+composer require --dev jasonmccreary/laravel-test-assertions
+
+echo '/draft.yaml' >> .gitignore
+echo '/.blueprint' >> .gitignore
+
+# Install Laravel Debugbar
+composer require --dev barryvdh/laravel-debugbar
