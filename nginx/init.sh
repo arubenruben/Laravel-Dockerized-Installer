50af08ab7206b903f889bc3e7e681b2d3ec55aac
Rúben Almeida <ruben.almeida@innovpoint.com>
2025-08-10 13:40:39 +0100

    Add logs to te init.sh file

--- nginx/init.sh
+++ nginx/init.sh
@@ -3,7 +3,13 @@
 # Exit on any error
 set -e
 
+echo "Substituting environment variables in Nginx configuration..."
+
 envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Run this script since it is mentioned as entrypoint for nginx base image
-exec /docker-entrypoint.sh
+echo "Running Nginx entrypoint script..."
+sh /docker-entrypoint.sh
+
+echo "Running Nginx..."
+exec "$@"
